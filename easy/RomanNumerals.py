1b5df41f7471ee960915bec2e9a5306c81691c2c
Jimmy Juarez <jimmyjuarez2021@gmail.com>
2024-01-27 21:45:48 -0500

    finished

--- easy/RomanNumerals.py
+++ easy/RomanNumerals.py
@@ -5,18 +5,22 @@ class Solution(object):
         :rtype: int
         """
         nums = list(s)
-        romanNums = dict({'I':1, 'V':5, 'X':10})
-        
+        romanNums = dict(
+            {
+                'I':1, 'V':5, 
+                'X':10, 'L':50,
+                'C':100, 'D':500,
+                'M':1000
+            })
+        res = 0
         for i in range(len(nums)):
-            print('nums', nums[i])
-        
-        print(nums)
-        print('length', int(len(nums)))
-        
-        if int(s) in romanNums:
-            return romanNums.get(int(s))
-        else:
-            return False
+            
+            if i > 0 and romanNums[nums[i]] > romanNums[nums[i - 1]]:
+                res +=  romanNums[nums[i]] - 2 * romanNums[nums[i - 1]]
+            else:
+                res += romanNums[nums[i]]
+            
+        return res
 
 obj = Solution()
-print(obj.romanToInt('1'))
+print(obj.romanToInt('XLIX'))
